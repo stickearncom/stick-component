ddf5909220af0b62d3795b9afb7e8c86c1541261
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-02-22 23:56:42 +0700

    add list item on scrow and scicon

--- src/components/foundations/Layout/SCRow/SCRow.stories.js
+++ src/components/foundations/Layout/SCRow/SCRow.stories.js
@@ -2,7 +2,11 @@ import SCRow from './index.vue'
 
 export default {
   title: 'Foundations/Layout/Grid/Row',
-  component: SCRow
+  component: SCRow,
+  argTypes: {
+    align: { control: { type: 'select', options: ['top', 'middle', 'bottom'] } },
+    justify: { control: { type: 'select', options: ['start', 'end', 'center', 'space-around', 'space-between'] } }
+  }
 }
 
 const Template = (args, { argTypes }) => ({
