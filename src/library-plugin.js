a90fdad3fa5078607de9cae91484c9aeaf843004
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-03-06 20:38:20 +0700

    add global plugin config

---
+++ src/library-plugin.js
@@ -0,0 +1,35 @@
+import './styles/main.scss'
+import './styles/fonts-face.scss'
+
+// Foundations
+import SCIcon from './components/foundations/SCIcon/index.vue'
+import SCCol from './components/foundations/Layout/SCCol/index.vue'
+import SCRow from './components/foundations/Layout/SCRow/index.vue'
+
+// Data Entry
+import SCInput from './components/DataEntry/SCInput/index.vue'
+
+// Components
+import SCButton from './components/SCButton/index.vue'
+
+const components = {
+  SCIcon,
+  SCCol,
+  SCRow,
+  SCInput,
+  SCButton
+}
+
+const install = (Vue) => {
+  Object.values(components).forEach((component) => {
+    const componentName = component.name
+    Vue.component(componentName, component)
+  })
+}
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default ({ ...components, install })
