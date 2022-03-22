d9a2ffd67ea2e8aa53984e09b79e13efabac8d5a
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-03-22 23:03:04 +0700

    update input pre post tab

--- src/components/data-entry/SCInput/example/SCInputPrePostTab.vue
+++ src/components/data-entry/SCInput/example/SCInputPrePostTab.vue
@@ -6,7 +6,7 @@
     <hr class="mb-4">
 
     <SCRow :gutter="[16, 32]">
-      <SCCol :span="6">
+      <SCCol :span="8">
         <SCInput
           addon-before="http://"
           addon-after=".com"
@@ -16,32 +16,174 @@
           allow-clear
         />
       </SCCol>
-      <SCCol :span="6">
+      <SCCol :span="8">
         <SCInput
           addon-before="http://"
           addon-after=".com"
           default-value="mysite"
+          placeholder="small"
           allow-clear
         />
       </SCCol>
-      <SCCol :span="6">
+      <SCCol :span="8">
         <SCInput
           addon-before="http://"
           addon-after=".com"
           default-value="mysite"
           size="large"
+          placeholder="small"
           allow-clear
         />
       </SCCol>
-      <SCCol :span="6">
+
+      <!-- Group Select -->
+      <SCCol :span="8">
+        <SCInput default-value="mysite" allow-clear size="small">
+          <SCSelect
+            slot="addonBefore"
+            default-value="Http://"
+            size="small"
+            style="width: 90px"
+          >
+            <SCSelectOption value="Http://">
+              Http://
+            </SCSelectOption>
+            <SCSelectOption value="Https://">
+              Https://
+            </SCSelectOption>
+          </SCSelect>
+          <SCSelect
+            slot="addonAfter"
+            default-value=".com"
+            size="small"
+            style="width: 80px"
+          >
+            <SCSelectOption value=".com">
+              .com
+            </SCSelectOption>
+            <SCSelectOption value=".jp">
+              .jp
+            </SCSelectOption>
+            <SCSelectOption value=".cn">
+              .cn
+            </SCSelectOption>
+            <SCSelectOption value=".org">
+              .org
+            </SCSelectOption>
+          </SCSelect>
+        </SCInput>
+      </SCCol>
+
+      <SCCol :span="8">
+        <SCInput default-value="mysite" allow-clear>
+          <SCSelect slot="addonBefore" default-value="Http://" style="width: 90px">
+            <SCSelectOption value="Http://">
+              Http://
+            </SCSelectOption>
+            <SCSelectOption value="Https://">
+              Https://
+            </SCSelectOption>
+          </SCSelect>
+          <SCSelect slot="addonAfter" default-value=".com" style="width: 80px">
+            <SCSelectOption value=".com">
+              .com
+            </SCSelectOption>
+            <SCSelectOption value=".jp">
+              .jp
+            </SCSelectOption>
+            <SCSelectOption value=".cn">
+              .cn
+            </SCSelectOption>
+            <SCSelectOption value=".org">
+              .org
+            </SCSelectOption>
+          </SCSelect>
+        </SCInput>
+      </SCCol>
+
+      <SCCol :span="8">
+        <SCInput default-value="mysite" allow-clear size="large">
+          <SCSelect
+            slot="addonBefore"
+            default-value="Http://"
+            size="large"
+            style="width: 90px"
+          >
+            <SCSelectOption value="Http://">
+              Http://
+            </SCSelectOption>
+            <SCSelectOption value="Https://">
+              Https://
+            </SCSelectOption>
+          </SCSelect>
+          <SCSelect
+            slot="addonAfter"
+            default-value=".com"
+            size="large"
+            style="width: 80px"
+          >
+            <SCSelectOption value=".com">
+              .com
+            </SCSelectOption>
+            <SCSelectOption value=".jp">
+              .jp
+            </SCSelectOption>
+            <SCSelectOption value=".cn">
+              .cn
+            </SCSelectOption>
+            <SCSelectOption value=".org">
+              .org
+            </SCSelectOption>
+          </SCSelect>
+        </SCInput>
+      </SCCol>
+
+      <!-- Disabled -->
+      <SCCol :span="8">
         <SCInput
           addon-before="http://"
           addon-after=".com"
           default-value="mysite"
-          size="large"
+          placeholder="small"
+          allow-clear
           disabled
-          allow-clear
         />
+      </SCCol>
+      <SCCol :span="8">
+        <SCInput default-value="mysite" allow-clear disabled>
+          <SCSelect
+            slot="addonBefore"
+            default-value="Http://"
+            disabled
+            style="width: 90px"
+          >
+            <SCSelectOption value="Http://">
+              Http://
+            </SCSelectOption>
+            <SCSelectOption value="Https://">
+              Https://
+            </SCSelectOption>
+          </SCSelect>
+          <SCSelect
+            slot="addonAfter"
+            default-value=".com"
+            disabled
+            style="width: 80px"
+          >
+            <SCSelectOption value=".com">
+              .com
+            </SCSelectOption>
+            <SCSelectOption value=".jp">
+              .jp
+            </SCSelectOption>
+            <SCSelectOption value=".cn">
+              .cn
+            </SCSelectOption>
+            <SCSelectOption value=".org">
+              .org
+            </SCSelectOption>
+          </SCSelect>
+        </SCInput>
       </SCCol>
     </SCRow>
   </div>
@@ -51,6 +193,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import SCCol from '@/components/foundations/Layout/SCCol/index.vue'
 import SCRow from '@/components/foundations/Layout/SCRow/index.vue'
+import SCSelect from '@/components/data-entry/SCSelect/index.vue'
+import SCSelectOption from '@/components/data-entry/SCSelect/SCSelectOption/index.vue'
 
 import SCInput from '../index.vue'
 
@@ -58,7 +202,9 @@ export default {
   components: {
     SCRow,
     SCCol,
-    SCInput
+    SCInput,
+    SCSelect,
+    SCSelectOption
   }
 }
 </script>
